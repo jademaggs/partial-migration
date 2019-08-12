0af544882b8a7e1b187185906b8dd1395c60fb05
Jade Maggs <jademaggs@gmail.com>
2019-08-12 21:30:25 +1200

    Update pm_model_movement.R

--- pm_model_movement.R
+++ pm_model_movement.R
@@ -126,47 +126,6 @@ prediction_bioregion <- cbind(prediction_bioregion, predict(m3, prediction_biore
 
 # DEVELOP GAUSSIAN MODEL OF MOVEMENT DIRECTION ----------------------------
 
-# Add polar bearing column to dataframe
-migrate.dat$bearing.deg <-
-  earth.bear(
-    migrate.dat$release_long,
-    migrate.dat$release_lat,
-    migrate.dat$recap_long,
-    migrate.dat$recap_lat
-  )
-
-# Convert bearing in degrees to radians
-migrate.dat$bearing <- migrate.dat$bearing * (pi/180)
-
-# Define function to convert polar bearing to cartesian coordinates
-polar2cart<-function(x,y, dist,bearing,as.deg=FALSE){
-  ## Translate Polar coordinates into Cartesian coordinates
-  ## based on starting location, distance, and bearing
-  ## as.deg indicates if the bearing is in degrees (T) or radians (F)
-  
-  if(as.deg){
-    ##if bearing is in degrees, convert to radians
-    bearing=bearing*pi/180
-  }
-  
-  newx<-x+dist*sin(bearing)  ##X
-  newy<-y+dist*cos(bearing)  ##Y
-  return(list("x"=newx,"y"=newy))
-}
-
-# Convert polar bearing to cartesian coordinates
-migrate.dat$cartesian_x <- polar2cart(0,0, 
-                                    migrate.dat$km_moved,
-                                    migrate.dat$bearing.deg, TRUE)$x
-
-migrate.dat$cartesian_y <- polar2cart(0,0, 
-                                      migrate.dat$km_moved,
-                                      migrate.dat$bearing.deg, TRUE)$y
-
-# Add azimuths
-migrate.dat$azimuth_y <- sin(migrate.dat$bearing)
-migrate.dat$azimuth_x <- cos(migrate.dat$bearing)
-
 # Run MetaNMDS and extract coefficients for GLM analysis
 migrate.matrix <- 
   subset(migrate.dat, select = c('cartesian_x', 'cartesian_y'))
